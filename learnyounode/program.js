5b4b1b755cbe45fd28d0fe2bfeaaedf3371d7246
vitaminvp <vitamin@ukr.net>
2019-09-08 21:58:27 +0300

    learnyounode: complete 3 tasks

---
+++ learnyounode/program.js
@@ -0,0 +1,18 @@
+// 1.
+// console.log("HELLO WORLD")
+
+// 2.
+// let sum = 0;
+// for(let i=2; i < process.argv.length; i++ ){
+//   sum += Number(process.argv[i])
+// }
+// console.log(sum);
+
+// 3.
+// var fs = require("fs");
+// buf = fs.readFileSync(process.argv[2]);
+// var str = buf.toString();
+// var strArray = str.split("\n");
+// console.log(strArray.length-1);
+
+// 4.
